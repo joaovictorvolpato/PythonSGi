cb8bea2aa2be9434343dfbc40de4e42328ae64b2
joao victor <jvv2003@gmail.com>
2024-03-14 12:29:29 -0300

    Hello World

---
+++ main.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QApplication
+import sys
+from src.View.View import View
+
+if (__name__ == "__main__"):
+    app = QApplication(sys.argv)
+    view = View()
+    sys.exit(app.exec_())
---
+++ src/View/View.py
@@ -0,0 +1,10 @@
+from src.Model.singleton import SingletonMeta
+from PyQt5.QtWidgets import QMainWindow, QLabel, QWidget
+
+
+class View(QWidget):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle("Hello World!")
+        self.show()
+
